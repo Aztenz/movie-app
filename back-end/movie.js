9d2a418860d806179445ad38a00112b2d1a78c9c
Aztenz <aztenz@outlook.com>
2023-09-03 01:36:26 +0300

    Update movie.js

---
+++ back-end/movie.js
@@ -0,0 +1,10 @@
+// models/movie.js
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  genre: String,
+  reviews: [{ text: String, rating: Number }],
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
